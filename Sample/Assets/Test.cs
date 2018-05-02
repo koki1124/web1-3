e7560f8ba11a0447a7c9c265e5f20e8f985f88d3
Unknown <koki11241124@gmail.com>
2018-05-02 18:05:04 +0900

    5月2日

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string str1 = "happy ";
+        string str2 = "birthday";
+        string message;
+
+        message = str1 + str2;
+
+        Debug.Log(message);
+
+       
+
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
